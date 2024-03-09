f689c29eed3c84df1d924f1c5585750a3bc553d1
fitzgallo <fitzgallo_gallo@rcc.edu.ph>
2024-03-09 13:58:55 +0800

    Math

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+   <head>
+   Fitzgerald B. Gallo
+   <p>ADET BSIT - II</p>
+
+
+
+    </head>
+    <body>
+    <?php
+ADDITION
+$x = 20;  
+$x += 100;
+
+echo $x;
+SUBTRACTION
+$x = 50;
+$x -= 30;
+
+echo $x;
+MULTIPLICATION
+$x = 5;
+$x *= 6;
+
+echo $x;
+DIVISION
+$x = 10;
+$x /= 5;
+
+echo $x;
+MODULUS
+$x = 15;
+$x %= 4;
+
+echo $x;
+?>  
+
+
+</body>
+
+</html>
